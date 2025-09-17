978c5ca120ce9511095d79bb872afb128603c037
saimjutt <saimjutt2280@gmail.com>
2025-09-17 22:13:43 +0500

    update repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base:"/Portfolio",
+      base:"/new-portfolio",
      
     };
 });
