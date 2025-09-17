b1bb600c18aac412fed0f44186fe6941e8e23345
saimjutt <saimjutt2280@gmail.com>
2025-09-17 22:35:20 +0500

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      base:"/new-portfolio",
+      base:"/new-portfolio/",
      
     };
 });
